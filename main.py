f4f8317d74fa5a9eb3480767c952e4991e2e2cea
Tochjosh <tochjosh@gmail.com>
2022-05-30 07:55:27 +0100

    chnages to main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import time
+
 from deepface import DeepFace
 from collect_images import collect
 
-# collect()
-real = DeepFace.stream('database', model_name='Facenet', detector_backend='dlib',
-                       distance_metric='euclidean', time_threshold=1, frame_threshold=1)
+
+def again():
+    response = input("Enter '1' for facial recognition or '2' to collect image sample: ")
+    if response == str(1):
+        DeepFace.stream('database', model_name='Facenet', detector_backend='dlib',
+                        distance_metric='euclidean', time_threshold=1, frame_threshold=1)
+    elif response == str(2):
+        collect()
+    else:
+        time.sleep(1)
+        print("wrong input")
+        again()
+
+
+again()
